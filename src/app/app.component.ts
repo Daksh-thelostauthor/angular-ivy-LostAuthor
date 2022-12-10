9ceceb74261725c2b9bba5fada3a8e44323c0085
Daksh-thelostauthor <77046620+Daksh-thelostauthor@users.noreply.github.com>
2022-12-10 13:01:44 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   toggle = true;
-  names;
+  public names;
   public holiday = [
     {
       holiday_name: 'Alex',
