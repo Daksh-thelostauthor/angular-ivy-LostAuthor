4f97812cd6124d35700ad006d1fbc5c06ed5581d
Daksh-thelostauthor <77046620+Daksh-thelostauthor@users.noreply.github.com>
2022-12-10 17:31:55 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { getLocaleCurrencySymbol } from '@angular/common';
 import { Component, VERSION } from '@angular/core';
 
 @Component({
@@ -8,32 +9,37 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   toggle = true;
   public names;
+  public days;
   public holiday = [
     {
       holiday_name: 'Alex',
       Status: 'OKay',
-      Number_Of_Days: '4',
+      Number_Of_Days: '454',
     },
     {
       holiday_name: 'Clair',
       Status: 'OKay',
-      Number_Of_Days: '4',
+      Number_Of_Days: '45',
     },
     {
       holiday_name: 'Zendaya',
       Status: 'OKay',
-      Number_Of_Days: '4',
+      Number_Of_Days: '44',
     },
     {
       holiday_name: 'Jessie Pinkman',
       Status: 'OKay',
-      Number_Of_Days: '4',
+      Number_Of_Days: '77',
     },
   ];
 
   holidaySelected(holidayObj) {
-    console.log(holidayObj);
     this.names = holidayObj.holiday_name;
+    this.days = holidayObj.Number_Of_Days;
     this.toggle = !this.toggle;
+  }
+
+  Clickit(i) {
+    console.log(i);
   }
 }
